cc8ca4e5cdad2fd95d25492b95c81b0dc7574863
晚风拂柳颜 <434857005@qq.com>
2024-03-17 15:29:28 +0800

    修复用户信息修改后没刷新数据的bug

--- dashboard/src/views/profile/components/Account.vue
+++ dashboard/src/views/profile/components/Account.vue
@@ -7,10 +7,10 @@
       <el-input v-model="form.nickname" placeholder="请输入昵称"/>
     </el-form-item>
     <el-form-item label="手机号码" prop="phone">
-      <el-input v-model="form.phone" maxlength="11" />
+      <el-input v-model="form.phone" maxlength="11"/>
     </el-form-item>
     <el-form-item label="邮箱" prop="email">
-      <el-input v-model="form.email" maxlength="50" />
+      <el-input v-model="form.email" maxlength="50"/>
     </el-form-item>
     <el-form-item label="性别" prop="sex">
       <el-radio-group v-model="form.sex">
@@ -24,14 +24,14 @@
     </el-form-item>
     <el-form-item>
       <el-button type="primary" size="mini" @click="submit">保存</el-button>
-<!--      <el-button type="danger" size="mini" @click="close">关闭</el-button>-->
+      <!--      <el-button type="danger" size="mini" @click="close">关闭</el-button>-->
     </el-form-item>
   </el-form>
 </template>
 
 <script>
 
-import { changeInfo } from "@/api/user";
+import {changeInfo, getInfo} from "@/api/user";
 
 export default {
   props: {
@@ -45,21 +45,21 @@ export default {
       // 表单校验
       rules: {
         username: [
-          { required: true, message: "用户名不能为空", trigger: "blur" }
+          {required: true, message: "用户名不能为空", trigger: "blur"}
         ],
         nickname: [
-          { required: true, message: "用户昵称不能为空", trigger: "blur" }
+          {required: true, message: "用户昵称不能为空", trigger: "blur"}
         ],
         email: [
-          { required: true, message: "邮箱地址不能为空", trigger: "blur" },
-          { type: "email", message: "'请输入正确的邮箱地址", trigger: ["blur", "change"] }
+          {required: true, message: "邮箱地址不能为空", trigger: "blur"},
+          {type: "email", message: "'请输入正确的邮箱地址", trigger: ["blur", "change"]}
         ],
         phone: [
-          { required: true, message: "手机号码不能为空", trigger: "blur" },
-          { pattern: /^1[2-9][0-9]\d{8}$/, message: "请输入正确的手机号码", trigger: "blur" }
+          {required: true, message: "手机号码不能为空", trigger: "blur"},
+          {pattern: /^1[2-9][0-9]\d{8}$/, message: "请输入正确的手机号码", trigger: "blur"}
         ],
         sex: [
-          { required: true, message: "性别不能为空", trigger: "blur" }
+          {required: true, message: "性别不能为空", trigger: "blur"}
         ]
       },
       // 性别状态字典
@@ -82,15 +82,23 @@ export default {
     submit() {
       this.$refs["form"].validate(valid => {
         if (valid) {
-          changeInfo(this.form).then( response => {
+          changeInfo(this.form).then(response => {
             this.msgSuccess("修改成功");
+            getInfo().then(resp => {
+              if (resp.code === 0) {
+                let user = resp.data;
+                for (let k in this.form) {
+                  this.form[k] = user.hasOwnProperty(k) ? user[k] : this.form[k]
+                }
+              }
+            })
           });
         }
       });
     },
     close() {
       this.$store.dispatch("tagsView/delView", this.$route);
-      this.$router.push({ path: "/index" });
+      this.$router.push({path: "/index"});
     }
   }
 };
